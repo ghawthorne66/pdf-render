c9d32737fdb9ce8c1b30b0bef68dd2124e74441e
Muhammad Ali <alipakistani90@gmail.com>
2020-05-23 02:51:46 +0500

    PDF Render

--- src/App.js
+++ src/App.js
@@ -2,23 +2,31 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import TestPDF from './pdf-files/test-file.pdf'
+
 function App() {
+
+  // const PdfBlob = new Blob(TestPDF);
+
+  // const file = new Blob(PdfBlob, {
+  //   type: 'application/pdf',
+  // });
+  
+  // const fileURL = URL.createObjectURL(file);
+  
+  // window.open(fileURL);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <main>
+        <h3>Testing React Apps</h3>
+        <br/>
+        <a href={TestPDF}>From Source Folder</a>
+
+        <a href="https://pdf-renderer.netlify.app/pdf-files/test-file.pdf">From Static Files</a>
+        {/* <a href="http://localhost:3000/pdf-files/viewer.html/test-file.pdf">Click here for my pdf</a> */}
+      </main>
     </div>
   );
 }
